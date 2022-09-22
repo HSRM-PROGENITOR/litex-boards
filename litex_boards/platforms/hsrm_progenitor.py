57252fbab674469a285e7418969b28955acca09e
Steffen Reith <Steffen.Reith@hs-rm.de>
2022-09-22 19:20:25 +0200

    Experimental version by David

--- litex_boards/platforms/hsrm_progenitor.py
+++ litex_boards/platforms/hsrm_progenitor.py
@@ -53,49 +53,64 @@ _io = [
     ),
 
     # GMII Ethernet
-    ("eth1_clocks_ext", 0,
-        Subsignal("tx", Pins("D19")),
-        Subsignal("gtx", Pins("C13")),
-        Subsignal("rx", Pins("D21")),
-        IOStandard("LVCMOS33")
-    ),
+#    ("eth1_clocks_ext", 0,
+#        Subsignal("tx", Pins("D19")),
+#        Subsignal("gtx", Pins("C13")),
+#        Subsignal("rx", Pins("D21")),
+#        IOStandard("LVCMOS33")
+#    ),
 #    ("eth1_clocks_ext", 0,
 #        Subsignal("tx", Pins("D15")),
 #        Subsignal("gtx", Pins("A13")),
 #        Subsignal("rx", Pins("A15")),
 #        IOStandard("LVCMOS33")
 #    ),
-    ("eth_clocks_ext", 0,
-        Subsignal("tx", Pins("M18")),
-        Subsignal("gtx", Pins("G18")),
-        Subsignal("rx", Pins("AB20")),
-        IOStandard("LVCMOS33")
-    ),
-    ("eth2_clocks_ext", 0,
+#    ("eth3_clocks_ext", 0,
+#        Subsignal("tx", Pins("M18")),
+#        Subsignal("gtx", Pins("G18")),
+#        Subsignal("rx", Pins("AB20")),
+#        IOStandard("LVCMOS33")
+#    ),
+    ("eth_clocks", 0,
         Subsignal("tx", Pins("H18")),
         Subsignal("gtx", Pins("N20")),
-        Subsignal("rx", Pins("K19")),
+        Subsignal("rx", Pins("K18")),
+        IOStandard("LVCMOS33")
+    ),
+    ("eth", 0,
+        Subsignal("rst_n",   Pins("G20")),
+        #Subsignal("int_n",   Pins("MISSING")),#TODO Ask about that pin to be added
+        Subsignal("mdc",     Pins("L15")),
+        Subsignal("mdio",    Pins("L14")),
+        Subsignal("rx_dv",   Pins("L21")),
+        Subsignal("rx_er",   Pins("K21")),
+        Subsignal("rx_data", Pins("H19 J19 L20 L19 K19 J22 H22 H20")),
+        Subsignal("tx_en",   Pins("M20")),
+        Subsignal("tx_er",   Pins("M15")),
+        Subsignal("tx_data", Pins("M13 L13 N22 M22 H17 K17 J17 M16")),
+        Subsignal("col",  Pins("K22")),
+        Subsignal("crs",  Pins("M21")),
         IOStandard("LVCMOS33")
     ),
 #    ("eth4_clocks_ext", 0,
 #        Subsignal("ref_clk", Pins("R4")),
 #        IOStandard("LVCMOS33"),
 #    ),    
-    ("eth", 1,
-        Subsignal("rst_n",   Pins("Y22")),
-        #Subsignal("int_n",   Pins("MISSING")),#TODO Ask about that pin to be added
-        Subsignal("mdc",     Pins("AA10")),
-        Subsignal("mdio",    Pins("AA11")),
-        Subsignal("rx_dv",   Pins("E22")),
-        Subsignal("rx_er",   Pins("T21")),
-        Subsignal("rx_data", Pins("D22 G22 G21 E21 F18 E18 U17 Y21")),
-        Subsignal("tx_en",   Pins("B21")),
-        Subsignal("tx_er",   Pins("B13")),
-        Subsignal("tx_data", Pins("D17 C17 C14 C15 E19 C19 C18 A21")),
-        Subsignal("col",  Pins("R17")),
-        Subsignal("crs",  Pins("U21")), 
-        IOStandard("LVCMOS33")
-    ),
+#    ("eth", 1,
+#        Subsignal("rst_n",   Pins("Y22")),
+#        #Subsignal("int_n",   Pins("MISSING")),#TODO Ask about that pin to be added
+#        Subsignal("mdc",     Pins("AA10")),
+#        Subsignal("mdio",    Pins("AA11")),
+#        Subsignal("rx_dv",   Pins("E22")),
+#        Subsignal("rx_er",   Pins("T21")),
+#        Subsignal("rx_data", Pins("D22 G22 G21 E21 F18 E18 U17 Y21")),
+#        Subsignal("tx_en",   Pins("B21")),
+#        Subsignal("tx_er",   Pins("B13")),
+#        Subsignal("tx_data", Pins("D17 C17 C14 C15 E19 C19 C18 A21")),
+#        Subsignal("col",  Pins("R17")),
+#        Subsignal("crs",  Pins("U21")), 
+#        IOStandard("LVCMOS33")
+#    ),
  #   ("eth", 1,
  #       Subsignal("rst_n",   Pins("A19")),
  #       #subsignal("int_n",   Pins("MISSING")),#TODO Ask about that pin to be added
@@ -111,36 +126,21 @@ _io = [
  #       Subsignal("crs",  Pins("D20")),
  #       IOStandard("LVCMOS33")
  #    ),
-    ("eth", 0,
-        Subsignal("rst_n",   Pins("G16")),
-        #Subsignal("int_n",   Pins("MISSING")),#TODO Ask about that pin to be added
-        Subsignal("mdc",     Pins("V19")),
-        Subsignal("mdio",    Pins("V18")),
-        Subsignal("rx_dv",   Pins("Y18")),
-        Subsignal("rx_er",   Pins("J20")),
-        Subsignal("rx_data", Pins("Y19 V17 W17 AA19 L16 K16 K13 K14")),
-        Subsignal("tx_en",   Pins("G17")),
-        Subsignal("tx_er",   Pins("J21")),
-        Subsignal("tx_data", Pins("G15 G13 H13 L18 N18 N19 H14 J14")),
-        Subsignal("col",  Pins("J15")),
-        Subsignal("crs",  Pins("H15")),
-        IOStandard("LVCMOS33")
-    ),
-    ("eth", 2,
-        Subsignal("rst_n",   Pins("G20")),
-        #Subsignal("int_n",   Pins("MISSING")),#TODO Ask about that pin to be added
-        Subsignal("mdc",     Pins("L15")),
-        Subsignal("mdio",    Pins("L14")),
-        Subsignal("rx_dv",   Pins("L21")),
-        Subsignal("rx_er",   Pins("K21")),
-        Subsignal("rx_data", Pins("H19 J19 L20 L19 K18 J22 H22 H20")),
-        Subsignal("tx_en",   Pins("M20")),
-        Subsignal("tx_er",   Pins("M15")),
-        Subsignal("tx_data", Pins("M13 L13 N22 M22 H17 K17 J17 M16")),
-        Subsignal("col",  Pins("K22")),
-        Subsignal("crs",  Pins("M21")),
-        IOStandard("LVCMOS33")
-    ),
+#    ("eth", 0,
+#        Subsignal("rst_n",   Pins("G16")),
+#        #Subsignal("int_n",   Pins("MISSING")),#TODO Ask about that pin to be added
+#        Subsignal("mdc",     Pins("V19")),
+#        Subsignal("mdio",    Pins("V18")),
+#        Subsignal("rx_dv",   Pins("Y18")),
+#        Subsignal("rx_er",   Pins("J20")),
+#        Subsignal("rx_data", Pins("Y19 V17 W17 AA19 L16 K16 K13 K14")),
+#        Subsignal("tx_en",   Pins("G17")),
+#        Subsignal("tx_er",   Pins("J21")),
+#        Subsignal("tx_data", Pins("G15 G13 H13 L18 N18 N19 H14 J14")),
+#        Subsignal("col",  Pins("J15")),
+#        Subsignal("crs",  Pins("H15")),
+#        IOStandard("LVCMOS33")
+#    ),
 #    ("eth", 4,
 #        Subsignal("rst_n",   Pins("N17")),
 #        Subsignal("rx_data", Pins("N13 N14")),
@@ -203,26 +203,15 @@ class Platform(XilinxPlatform):
     def do_finalize(self, fragment):
         XilinxPlatform.do_finalize(self, fragment)
         self.add_period_constraint(self.lookup_request("clk50",        loose=True), 1e9/50e6)        
-        self.add_platform_command("set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets eth_clocks_ext0_rx_IBUF]")
-        self.add_platform_command("set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets eth1_clocks_ext0_rx_IBUF]")
-        self.add_platform_command("set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets eth2_clocks_ext0_rx_IBUF]")
-        self.add_platform_command("set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets main_ethphy_eth_tx_clk_reg]")
-        self.add_platform_command("set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets main_ethphy1_eth_tx_clk_reg]")
-        self.add_platform_command("set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets main_ethphy2_eth_tx_clk_reg]")
-        #self.add_platform_command("set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets main_ethphy3_eth_tx_clk_reg]")
-        #self.add_platform_command("set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets eth3_clocks_ext0_rx_IBUF]")
-        #self.add_platform_command("set_property PULLUP TRUE [get_ports eth0_mdio]")
-        #self.add_platform_command("set_property PULLUP TRUE [get_ports eth1_mdio]")
-        #self.add_platform_command("set_property PULLUP TRUE [get_ports eth2_mdio]")      
-        self.add_period_constraint(self.lookup_request("eth_clocks_ext:gtx", loose=True), 1e9/125e6)
-        self.add_period_constraint(self.lookup_request("eth_clocks_ext:tx", loose=True), 1e9/125e6)
-        self.add_period_constraint(self.lookup_request("eth_clocks_ext:rx", loose=True), 1e9/125e6)
-        self.add_period_constraint(self.lookup_request("eth1_clocks_ext:gtx", loose=True), 1e9/125e6)
-        self.add_period_constraint(self.lookup_request("eth1_clocks_ext:tx", loose=True), 1e9/125e6)
-        self.add_period_constraint(self.lookup_request("eth1_clocks_ext:rx", loose=True), 1e9/125e6)
-        self.add_period_constraint(self.lookup_request("eth2_clocks_ext:gtx", loose=True), 1e9/125e6)
-        self.add_period_constraint(self.lookup_request("eth2_clocks_ext:tx", loose=True), 1e9/125e6)
-        self.add_period_constraint(self.lookup_request("eth2_clocks_ext:rx", loose=True), 1e9/125e6)
+        self.add_period_constraint(self.lookup_request("eth_clocks:gtx", loose=True), 1e9/125e6)
+        self.add_period_constraint(self.lookup_request("eth_clocks:tx", loose=True), 1e9/125e6)
+        self.add_period_constraint(self.lookup_request("eth_clocks:rx", loose=True), 1e9/125e6)
+#        self.add_period_constraint(self.lookup_request("eth1_clocks_ext:gtx", loose=True), 1e9/125e6)
+#        self.add_period_constraint(self.lookup_request("eth1_clocks_ext:tx", loose=True), 1e9/125e6)
+#        self.add_period_constraint(self.lookup_request("eth1_clocks_ext:rx", loose=True), 1e9/125e6)
+#        self.add_period_constraint(self.lookup_request("eth2_clocks_ext:gtx", loose=True), 1e9/125e6)
+#        self.add_period_constraint(self.lookup_request("eth2_clocks_ext:tx", loose=True), 1e9/125e6)
+#        self.add_period_constraint(self.lookup_request("eth2_clocks_ext:rx", loose=True), 1e9/125e6)
         #self.add_period_constraint(self.lookup_request("eth3_clocks_ext:gtx", loose=True), 1e9/125e6)
         #self.add_period_constraint(self.lookup_request("eth3_clocks_ext:tx", loose=True), 1e9/125e6)
         #self.add_period_constraint(self.lookup_request("eth3_clocks_ext:rx", loose=True), 1e9/125e6)
